0f68a6f1ee2368829b5f6c655594addb08362e0f
valeryi <valeryi.oleinyk@gmail.com>
2020-03-29 02:56:03 +0200

    Added Winston logging util

--- src/middlewares/test.ts
+++ src/middlewares/test.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
+import { Router, Request } from 'express';
 import { Middleware } from '.';
+// import { logger } from '../utils/winston';
 
 export const test: Middleware = (router: Router) => {
-    router.use((_req: any, _res: any, next: any) => {
-        // console.log('middleware is active');
+    router.use((_req: Request, _res: any, next: any) => {
+        // logger.info('middleware is active');
         next();
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { applyGraphQLMiddleware } from './graphql';
 import { applyExpressMiddleware, wrappers } from './middlewares/index';
 import { applyErrorHandlers } from './errorHandlers';
 import * as errorHandlers from './errorHandlers/handlers';
+import { sysLog } from './utils/winston';
 
 process.on("uncaughtException", e => { // LEARN: learn more about process and its properties and methods 
     console.log(e);
@@ -22,7 +23,7 @@ applyGraphQLMiddleware(app);
 applyErrorHandlers(app, errorHandlers);
 
 app.listen(port, () => {
-    console.info(`Server running at: http://localhost:${port}/graphql`)
+    sysLog.info(`Server running at: http://localhost:${port}/graphql`)
 });
 
 // Here I receive all necessary params and data for setting up an express / apollo server 
